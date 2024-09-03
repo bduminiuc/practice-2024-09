4e8452dbbcb3c572be9519e3f478b9884f025b7a
EgorKalinnikov <calinnicov@gmail.com>
2024-09-03 19:07:15 +0300

    Fixes

--- Vector.cpp
+++ Vector.cpp
@@ -4,167 +4,175 @@
 class Custom_Vector
 {
 private:
-    size_t size;
-    size_t capacity;
+    size_t _size{};
+    size_t _capacity{};
     int *buffer;
 
 public:
     Custom_Vector()
-        : size(0), capacity(0), buffer(nullptr) {} // конструктор по умолчанию
-    Custom_Vector(int size)
-        : size(0),
-          capacity(size),
-          buffer(new int[capacity]) {} // конструктор с заданным размером
+        : _size(0),
+          _capacity(0),
+          buffer(nullptr) {}
+
+    explicit Custom_Vector(size_t size)
+        : _size(size),
+          _capacity(size),
+          buffer(new int[_capacity])
+    {
+        std::fill_n(buffer, _size, int());
+    }
+
     Custom_Vector(const std::initializer_list<int> list)
-        : size(list.size()), capacity(list.size()), buffer(new int[capacity])
+        : Custom_Vector(list.size())
     {
-        for (size_t i = 0; i < size; i++)
-        {
-            buffer[i] = *(list.begin() + i);
-        }
-    } // Конструктор со списком инициализации
+        std::copy(list.begin(), list.end(), buffer);
+    }
+
     Custom_Vector(const Custom_Vector &other)
-        : size(other.size),
-          capacity(other.capacity),
-          buffer(new int[capacity])
+        : Custom_Vector(other._size)
     {
-        for (size_t i = 0; i < size; i++)
-        {
-            buffer[i] = other.buffer[i];
-        }
-    } // Конструктор копирования
+        std::copy(other.buffer, other.buffer + other._size, buffer);
+    }
+
     Custom_Vector(Custom_Vector &&other)
-        : size(other.size),
-          capacity(other.capacity),
-          buffer(new int[capacity])
+        : Custom_Vector()
     {
-        buffer = other.buffer;
-        other.buffer = nullptr;
-        other.size = other.capacity = 0;
-    } // Конструктор перемещения
-    ~Custom_Vector() { delete[] buffer; } // Деструктор
-    size_t get_size()
+        std::swap(buffer, other.buffer);
+        std::swap(_size, other._size);
+        std::swap(_capacity, other._capacity);
+    }
+
+    ~Custom_Vector() { delete[] buffer; }
+
+    size_t size() const
     {
-        return size;
-    } // Получение количества заполненных ячеек
-    size_t get_capacity()
+        return _size;
+    }
+
+    size_t capacity() const
     {
-        return capacity;
-    } // Получение количества всех ячеек
+        return _capacity;
+    }
+
     void assign(size_t count, const int &value)
     {
-        if (count > capacity)
-        {
-            reserve(count);
-        }
-        if (count > size)
-        {
-            size = count;
-        }
-        for (size_t i = 0; i < count; i++)
-        {
-            buffer[i] = value;
-        }
+        clear();
+        reserve(count);
+
+        _size = count;
+
+        std::fill_n(buffer, _size, value);
     }
+
     void assign(std::initializer_list<int> list)
     {
-        if (list.size() > capacity)
-        {
-            reserve(list.size());
-        }
-        if (list.size() > size)
-        {
-            size = list.size();
-        }
-        for (size_t i = 0; i < list.size(); i++)
-        {
-            buffer[i] = *(list.begin() + i);
-        }
+        clear();
+
+        reserve(list.size());
+
+        _size = list.size();
+
+        std::copy(list.begin(), list.end(), buffer);
     }
+
     void clear()
     {
-        std::destroy(buffer, buffer + size);
-        size = 0;
-    } // обнуление всех ячеек в массиве
+        _size = 0;
+    }
+
     void push_back(const int &element)
     {
-        if (capacity == 0)
+        if (_capacity == 0)
         {
-            reserve(8);
+            reserve(1);
         }
-        if (size == capacity)
+        if (_size == _capacity)
         {
-            reserve(2 * capacity);
+            reserve(2 * _capacity);
         }
-        buffer[size] = element;
-        size++;
-    } // вставка элемента в конец массива
+
+        buffer[_size] = element;
+        _size++;
+    }
+
     void reserve(size_t size_n)
     {
-        if (size_n <= capacity)
+        if (size_n <= _capacity)
         {
             return;
         }
+
         int *point = new int[size_n];
-        for (size_t i = 0; i < size; i++)
-        {
-            point[i] = buffer[i];
-        }
+
+        std::copy(buffer, buffer + _size, point);
+
         delete[] buffer;
-        buffer = point;
-        capacity = size_n;
-    } // Выделение памяти, если закончилось место
+        std::swap(buffer, point);
+        _capacity = size_n;
+    }
+
     int at(size_t pos)
     {
-        if (size < pos)
+        if (_size - 1 < pos || _size == 0)
         {
             throw std::out_of_range("Error");
         }
         return buffer[pos];
-    } // Возвращает указанный элемент с проверкой на границы
-    int &operator[](size_t pos)
+    }
+
+    const int &at(size_t pos) const
     {
-        return buffer[pos];
-    } // Возвращает указанный элемент
+        if (_size - 1 < pos || _size == 0)
+        {
+            throw std::out_of_range("Error");
+        }
+        else
+        {
+            return buffer[pos];
+        }
+    }
+
+    int &operator[](size_t pos) { return buffer[pos]; }
+    const int &operator[](size_t pos) const { return buffer[pos]; }
+
     Custom_Vector &operator=(const Custom_Vector &other)
     {
         if (this == &other)
         {
             return *this;
         }
-        if (other.size <= capacity)
+
+        if (other._size <= _capacity)
         {
             clear();
-            for (size_t i = 0; i < other.size; i++)
-            {
-                buffer[i] = other.buffer[i];
-            }
-            size = other.size;
+            std::copy(other.buffer, other.buffer + other._size, buffer);
+            _size = other._size;
             return *this;
         }
-        int *point = new int[other.capacity];
-        for (size_t i = 0; i < other.size; i++)
-        {
-            point[i] = other.buffer[i];
-        }
+
+        int *point = new int[other._capacity];
+        std::copy(other.buffer, other.buffer + other._size, point);
+
         delete[] buffer;
-        size = other.size;
-        capacity = other.capacity;
-        buffer = point;
+        _size = other._size;
+        _capacity = other._capacity;
+        std::swap(buffer, point);
         return *this;
-    } // Опреатор присваивания
+    }
+
     void erase(size_t pos)
     {
-        if (size < pos)
+        if (_size < pos || _size == 0)
         {
             throw std::out_of_range("Error");
         }
-        for (size_t i = pos; i < size; i++)
+
+        for (size_t i = pos; i < _size; i++)
         {
             buffer[i] = buffer[i + 1];
         }
-        size--;
-    } // Удалени�� элемента в массиве
+        _size--;
+    }
 };
 
 int main()
@@ -173,26 +181,26 @@ int main()
     assert(a[0] == 1);
     assert(a.at(2) == 7);
     // assert(a.at(5));//выкинет исключение
-    assert(a.get_size() == 4);
-    assert(a.get_capacity() == 4);
+    assert(a.size() == 4);
+    assert(a.capacity() == 4);
     a.push_back(5);
-    assert(a.get_size() == 5);
-    assert(a.get_capacity() == 8);
+    assert(a.size() == 5);
+    assert(a.capacity() == 8);
     assert(a[4] == 5);
     Custom_Vector b = a;
-    for (size_t i = 0; i < b.get_size(); i++)
+    for (size_t i = 0; i < b.size(); i++)
     {
         std::cout << b[i] << " ";
     }
     std::cout << std::endl;
     b.clear();
-    for (size_t i = 0; i < a.get_size(); i++)
+    for (size_t i = 0; i < a.size(); i++)
     {
         std::cout << a[i] << " ";
     }
     std::cout << std::endl;
     a.erase(2);
-    for (size_t i = 0; i < a.get_size(); i++)
+    for (size_t i = 0; i < a.size(); i++)
     {
         std::cout << a[i] << " ";
     }
@@ -200,31 +208,31 @@ int main()
     a.clear();
     Custom_Vector c;
     c.assign(4, 9);
-    for (size_t i = 0; i < c.get_size(); i++)
+    for (size_t i = 0; i < c.size(); i++)
     {
         std::cout << c[i] << " ";
     }
     std::cout << std::endl;
     c.assign(2, 7);
-    for (size_t i = 0; i < c.get_size(); i++)
+    for (size_t i = 0; i < c.size(); i++)
     {
         std::cout << c[i] << " ";
     }
     std::cout << std::endl;
     c.assign({1, 2, 3, 4, 5, 6});
-    for (size_t i = 0; i < c.get_size(); i++)
+    for (size_t i = 0; i < c.size(); i++)
     {
         std::cout << c[i] << " ";
     }
     std::cout << std::endl;
     c.erase(2);
-    for (size_t i = 0; i < c.get_size(); i++)
+    for (size_t i = 0; i < c.size(); i++)
     {
         std::cout << c[i] << " ";
     }
     std::cout << std::endl;
     c.clear();
-    assert(c.get_size() == 0);
-    assert(c.get_capacity() == 6);
+    assert(c.size() == 0);
+    assert(c.capacity() == 6);
     return 0;
 }
